9e97e075c2ef3c1b2b47b23a4d306d3967888e69
Bruce Becker <brucellino@gmail.com>
2018-05-31 16:11:08 +0200

    update vars for repos

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -13,6 +13,24 @@ def test_hosts_file(host):
     assert f.user == 'root'
     assert f.group == 'root'
 
+
 # Test that UMD release is there and that it has the right version
+def test_umd_version(host):
+    umd_package = host.package('umd-release')
+    assert umd_package.is_installed
+    assert umd_package.version.startswith("4")
+
 
 # Test that repositories are properly configured
+def test_repositories(host):
+
+    if (host.system_info.distribution == 'debian'):
+        print "it's a debian daddy"
+        # debian stuff
+    
+    if(host.system_info.distribution == 'redhat'):
+        print "it's a redhat daddy"
+        if(host.system_info.release >= '6'):
+            print str(host.system_info.release)
+    
+    print str(host.system_info.distribution)
